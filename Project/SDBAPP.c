793a81f984ce10381a36ab733dc2ee8ac540d46f
0SANDMAN1 <eslamehab2025@gmail.com>
2023-08-22 22:11:11 +0300

    My First Project

---
+++ Project/SDBAPP.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include "SDB.h"
+
+
+Bool SDB_IsFull();
+uint8 SDB_GetUsedSize();
+Bool SDB_AddEntry();
+void SDB_DeleteEntry(uint32 id);
+Bool SDB_ReadEntry(uint32 id);
+void SDB_GetList(uint8 *count, uint32 *list);
+Bool SDB_IsIdExist(uint32 id);
+void SDB_action(uint8 choice);
+
+
+
+void SDB_APP() {
+    uint8 choice;
+
+    do {
+        printf("       d8888888b     d888888888888888888888 \n");
+        printf("      d888888888b   d8888  888      888     \n");
+        printf("     d88P88888888b.d88888  888      888     \n");
+        printf("    d88P 888888Y88888P888  888      888     \n");
+        printf("   d88P  888888 Y888P 888  888      888     \n");
+        printf("  d88P   888888  Y8P  888  888      888     \n");
+        printf(" d8888888888888   \"   888  888      888     \n");
+        printf("\nStudent Database Menu:\n");
+        printf("1. Add Entry\n");
+        printf("2. Get Used Size\n");
+        printf("3. Read Student Data\n");
+        printf("4. Get List of Student IDs\n");
+        printf("5. Check if ID Exists\n");
+        printf("6. Delete Student Data\n");
+        printf("7. Check if Database is Full\n");
+        printf("0. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%hhu", &choice);
+
+        if (choice != 0) {
+            SDB_action(choice);
+        }
+    } while (choice != 0);
+}
+
+void SDB_action(uint8 choice) { // I created some variables in which they will be inputs to get passed to my action functions
+    uint32 id;
+    uint8 count;
+    uint32 list[MAX_STUDENTS];
+
+    switch (choice) {
+        case 1:
+            if (SDB_AddEntry()) {
+                printf("Student added successfully.\n");
+            } else {
+                printf("Unable to add student. Database is full.\n");
+            }
+            break;
+        case 2:
+            printf("Used size in database: %u\n", SDB_GetUsedSize());
+            break;
+        case 3:
+            printf("Enter Student ID to read: ");
+            scanf("%u", &id);
+            if (SDB_ReadEntry(id)) { // the ReadEntry() function returns true if the entry was read successfully
+                printf("Student data read successfully.\n");
+            } else {
+                printf("Student data not found.\n");
+            }
+            break;
+        case 4:
+            SDB_GetList(&count, list);
+            printf("List of Student IDs:\n");
+            for (uint8 i = 0; i < count; ++i) {
+                printf("%u ", list[i]);
+            }
+            printf("\n");
+            break;
+        case 5:
+            printf("Enter Student ID to check: ");
+            scanf("%u", &id);
+            if (SDB_IsIdExist(id)) { // the IsIdExist() function returns true if the student is already in the list of students otherwise it returns false
+                printf("Student ID exists in the database.\n");
+            } else {
+                printf("Student ID does not exist in the database.\n");
+            }
+            break;
+        case 6:
+            printf("Enter Student ID to delete: ");
+            scanf("%u", &id);
+            SDB_DeleteEntry(id);
+            break;
+        case 7:
+            if (SDB_IsFull()) { // the IsFull() function returns true if the database is full and the user has permission to delete the student from the database otherwise it returns false
+                printf("Database is full.\n");
+            } else {
+                printf("Database is not full.\n");
+            }
+            break;
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
+            break;
+    }
+}
+
+int main() {
+    SDB_APP();
+    return 0;
+}
---
+++ Project/STD.h
@@ -0,0 +1,13 @@
+#ifndef STD_H
+#define STD_H
+#define MAX_STUDENTS 10
+
+typedef unsigned char uint8; // byte: 1 range: 0 to 255 format specifier %c
+typedef char Int8; // byte: 1 range: -128 to 127 format specifier %c
+typedef unsigned short uint16; // byte: 2 range: 0 to 65535 format specifier %hu
+typedef short int16; // byte: 2 range: -32,768 to 32,767 format specifier %hd
+typedef unsigned int uint32; // byte: 4 range: 0 to 4,294,967,295  format specifier %u
+typedef int int32; // byte: 4 range: -2,147,483,648 to 2,147,483,647  format specifier %d
+typedef enum { FALSE, TRUE } Bool; // Conventionally, use uppercase for enum members
+
+#endif
