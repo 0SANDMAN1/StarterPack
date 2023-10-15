50a58e7d0e3474f5be30dd36a002dcd7dd1e0dec
0SANDMAN1 <eslamehab2025@gmail.com>
2023-10-15 18:10:31 +0300

    duplicates

--- MicroChip Projects/Led Spread/bit_math.h
+++
@@ -1,4 +0,0 @@
-#define Set(Reg, Pin) Reg |= (1 << Pin)
-#define Clr(Reg, Pin) Reg &=~ (1 << Pin)
-#define Toggle(Reg, Pin) Reg ^= (1 << Pin)
-#define Get(Reg, Pin) Reg &= (1 << Pin)
--- MicroChip Projects/Toggle LED/Toggle LED/bit_math.h
+++
@@ -1,5 +0,0 @@
-#define Set(Reg, Pin) Reg |= (1 << Pin)
-#define Clr(Reg, Pin) Reg &=~ (1 << Pin)
-#define Toggle(Reg, Pin) Reg ^= (1 << Pin)
-//#define Get(Reg,Bit) ((Reg>>Bit)&1)
-#define Get(Reg,Bit)    (Reg&(1<<Bit))>>Bit
